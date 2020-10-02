41b914c394e6dc0f2541d8398c8f29f596cd2cb7
Mark Chukwuebuka <markedah9652@gmail.com>
2020-10-02 15:29:49 +0100

    BackEnd Functionality

---
+++ ToDo/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from django.forms import ModelForm
+from .models import *
+
+class TaskForm(forms.ModelForm):
+    title = forms.CharField(widget = forms.TextInput(attrs = {'placeholder' : 'Enter Task ...'}), label = False)
+    due = forms.CharField(widget = forms.TextInput(attrs = {'placeholder' : 'Due date ...'}), label = False)
+
+    class Meta:
+        model = Task
+        fields = ['title', 'due']
+
+
+class UpdateForm(forms.ModelForm):
+    title = forms.CharField(widget=forms.TextInput(attrs = {'placeholder' : 'Enter Task ...'}))
+
+    class Meta:
+        model = Task
+        fields = ['title', 'due', 'complete']
