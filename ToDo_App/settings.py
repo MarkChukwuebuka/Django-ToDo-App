df027143be218f1df910ebceae1fcc5f6620c762
Mark Chukwuebuka <markedah9652@gmail.com>
2020-10-18 01:01:37 +0100

    environment variables

--- ToDo_App/settings.py
+++ ToDo_App/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ktsxy31jq2s_2u^=*p7!d800_4p++2u*djebuie39$alnjrv@7'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
